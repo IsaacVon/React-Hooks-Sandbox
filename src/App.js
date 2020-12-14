e45ff97f2dc35cc341b2646c8062b99a16cd80a2
Isaac Householder <isaachouseholder@gmail.com>
2020-12-13 19:13:34 -0800

    useState Basic Added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
+import React, { useState } from "react";
 import "./App.css";
 
 function App() {
-  return <p>Test </p>;
+  const [count, setCount] = useState(4);
+  const [theme, setTheme] = useState("blue");
+
+  function decrementCount() {
+    setCount((prevCount) => prevCount - 1);
+    setTheme("black");
+  }
+
+  function incrementCount() {
+    setCount((prevCount) => prevCount + 1);
+    setTheme("red");
+  }
+
+  return (
+    <div className="container">
+      <button onClick={decrementCount}>-</button>
+      <span>{count}</span>
+      <span>{theme}</span>
+      <button onClick={incrementCount}>+</button>
+    </div>
+  );
 }
 
 export default App;
